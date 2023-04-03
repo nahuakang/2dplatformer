57712a3fb420bf53876fe3392a4c56c60049e00d
Nahua Kang <kangnahua@gmail.com>
2023-04-03 19:05:26 +0200

    Add coin collection and total coin feature

--- scenes/BaseLevel.gd
+++ scenes/BaseLevel.gd
@@ -1,13 +1,27 @@
 extends Node
 
+signal coin_total_changed
+
 var player_scene = preload("res://scenes/Player.tscn")
 var spawn_position = Vector2.ZERO
 var current_player_node = null
+var total_coins = 0
+var collected_coins = 0
 
 func _ready() -> void:
 	spawn_position = $Player.global_position
 	register_player($Player)
 	
+	coin_total_changed(get_tree().get_nodes_in_group("coin").size())
+
+func coin_collected() -> void:
+	collected_coins += 1
+	emit_signal("coin_total_changed", total_coins, collected_coins)
+
+func coin_total_changed(new_total: int) -> void:
+	total_coins = new_total
+	emit_signal("coin_total_changed", total_coins, collected_coins)
+
 func register_player(player: KinematicBody2D):
 	current_player_node = player
 	current_player_node.connect("died", self, "on_player_died", [], CONNECT_DEFERRED)
--- scenes/Coin.gd
+++ scenes/Coin.gd
@@ -6,7 +6,8 @@ func _ready() -> void:
 func on_area_entered(area2d: Area2D):
 	$AnimationPlayer.play("pickup")
 	call_deferred("disable_pickup")
-	
+	var base_level = get_tree().get_nodes_in_group("base_level")[0]
+	base_level.coin_collected()
 
 func disable_pickup() -> void:
 	$Area2D/CollisionShape2D.disabled = true
