7d9df772a2560d6405f53309fb3bae872e3e2bac
Nahua Kang <kangnahua@gmail.com>
2023-04-14 22:30:41 +0200

    Enrich LevelComplete UI with panel and button textures

--- scenes/UI/LevelComplete.gd
+++ scenes/UI/LevelComplete.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 func _ready():
-	$MarginContainer/VBoxContainer/Button.connect("pressed", self, "on_next_button_pressed")
+	$PanelContainer/MarginContainer/VBoxContainer/Button.connect("pressed", self, "on_next_button_pressed")
 
 func on_next_button_pressed() -> void:
 	$"/root/LevelManager".increment_level()
